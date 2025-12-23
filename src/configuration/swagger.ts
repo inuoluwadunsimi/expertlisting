5dc82f9748ba18e90bba204a4fa2d98d411e95fb
Inuoluwadunsimi <danielolaoladeinde@gmail.com>
2025-12-23 10:19:55 +0100

    fix(doc): use module declaration for swagger file

--- src/configuration/swagger.ts
+++ src/configuration/swagger.ts
@@ -1,17 +1,16 @@
-import swaggerJSDoc from 'swagger-jsdoc';
+const swaggerJSDoc = require('swagger-jsdoc');
 
 const options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'typescript-express boilerplate',
+      title: 'Experlisting API',
       version: '1.0.0',
       description: 'Docs',
     },
   },
 
-  apis:['./spec/api.yaml']
+  apis: ['./spec/api.yaml'],
 };
 
-
-export default swaggerJSDoc(options)
+module.exports = swaggerJSDoc(options);
